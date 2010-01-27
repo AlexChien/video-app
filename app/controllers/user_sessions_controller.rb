8dc5b59fb778aa5ae5d09f86e8fc4bc345b01dc8
stainless <johnsonqu@gmail.com>
2010-01-27 16:58:20 +0800

    redirect to home page after login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,7 +11,8 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default account_url
+      # redirect_back_or_default account_url
+      redirect_back_or_default '/'
     else
       render :action => :new
     end
@@ -20,7 +21,8 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    # redirect_back_or_default new_user_session_url
+    redirect_back_or_default '/'
   end
 end
 
